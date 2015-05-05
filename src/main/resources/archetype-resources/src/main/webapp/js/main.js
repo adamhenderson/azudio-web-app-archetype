53a540fd9cad29272686a4fc318755d7129dd4da
Adam Henderson <adamhenderson@azudio.co.uk>
2015-05-05 21:42:28 +0100

    Included the Rest module

--- src/main/resources/archetype-resources/src/main/webapp/js/main.js
+++ src/main/resources/archetype-resources/src/main/webapp/js/main.js
@@ -1,5 +1,5 @@
 var startTime = new Date();
-require([ "dojo/ready", "app/Application", "dojo/parser", "app/ExampleGUI" ], function(ready, Application) {
+require([ "dojo/ready", "app/Application", "dojo/parser", "app/ExampleGUI","app/Rest" ], function(ready, Application) {
     ready(function() {
         Application.init();
     });
