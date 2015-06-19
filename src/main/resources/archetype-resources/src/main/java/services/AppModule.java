a04ad13493bcc41e4ab8ad7eb863c16723d904f7
Adam Henderson <adamhenderson@azudio.co.uk>
2015-06-19 18:46:37 +0100

    Added Execution Mode Modules

--- src/main/resources/archetype-resources/src/main/java/services/AppModule.java
+++ src/main/resources/archetype-resources/src/main/java/services/AppModule.java
@@ -30,98 +30,58 @@ import ${package}.services.rest.PersonResourceJPA;
 @ImportModule({ /* Uncomment to enable Atmosphere: AtmosphereModule.class */})
 public class AppModule {
 
-	private static final Logger log = LoggerFactory.getLogger(AppModule.class);
+    private static final Logger log = LoggerFactory.getLogger(AppModule.class);
 
-	@Contribute(EntityManagerSource.class)
-	public static void configurePersistenceUnitInfos(MappedConfiguration<String, PersistenceUnitConfigurer> cfg) {
+    // Contributions to EntityManagerSource are in the [MODE]ModeModule.java files
+    // IntegrationModeModule.java
+    // UatModeModule.java
+    // ProductionModeModule.java - This module will be loaded if the tapestry.execution-mode JVM property is not set
+    
+    /**
+     * Contribute to the Application Defaults
+     * 
+     * @param configuration
+     */
+    public static void contributeApplicationDefaults(MappedConfiguration<String, Object> configuration) {
+        configuration.add(SymbolConstants.ENABLE_HTML5_SUPPORT, true);
+        configuration.add(SymbolConstants.FORM_CLIENT_LOGIC_ENABLED, false);
+        configuration.add(SymbolConstants.INCLUDE_CORE_STACK, false);
+        configuration.add(SymbolConstants.ENABLE_PAGELOADING_MASK, false);
+        configuration.add(SymbolConstants.COMBINE_SCRIPTS, true);
+        configuration.add(IOCSymbols.THREAD_POOL_CORE_SIZE, 7);
+    }
 
-		log.debug("Config Persistence");
+    /**
+     * Binds Services
+     * 
+     * @param binder
+     */
+    public static void bind(ServiceBinder binder) {
+        binder.bind(AppCore.class, AppCoreImpl.class);
+        binder.bind(PersonResource.class, PersonResourceJPA.class);
+    }
 
-		PersistenceUnitConfigurer configurer = new PersistenceUnitConfigurer() {
+    /**
+     * REST: Contribute the proxies services for Live Class Reloading
+     * 
+     * @param singletons
+     * @param personResource
+     */
+    @Contribute(javax.ws.rs.core.Application.class)
+    public static void configureRestResources(Configuration<Object> singletons, PersonResource personResource) {
+        singletons.add(personResource);
+    }
 
-			// /**
-			// * MYSQL Implementation
-			// * @param unitInfo
-			// */
-			// public void configure(TapestryPersistenceUnitInfo unitInfo) {
-			// unitInfo.persistenceProviderClassName("org.hibernate.ejb.HibernatePersistence");
-			// unitInfo.transactionType(PersistenceUnitTransactionType.RESOURCE_LOCAL);
-			// unitInfo.addProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
-			// unitInfo.addProperty("hibernate.connection.url", "jdbc:mysql://localhost/${artifactId}");
-			// unitInfo.addProperty("hibernate.connection.username", "sample18");
-			// unitInfo.addProperty("hibernate.connection.password", "sample18");
-			// unitInfo.addProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
-			// unitInfo.addProperty("hbm2ddl.auto", "create");
-			// }
-
-			/**
-			 * Embedded HSQLDB Implementation
-			 * 
-			 * @param unitInfo
-			 */
-		    @Override
-			public void configure(TapestryPersistenceUnitInfo unitInfo) {
-				unitInfo.persistenceProviderClassName("org.hibernate.ejb.HibernatePersistence");
-				unitInfo.transactionType(PersistenceUnitTransactionType.RESOURCE_LOCAL);
-				unitInfo.addProperty("hibernate.connection.driver_class", "org.hsqldb.jdbcDriver");
-				unitInfo.addProperty("hibernate.connection.url", "jdbc:hsqldb:file:database/${artifactId}");
-				unitInfo.addProperty("hibernate.connection.username", "SA");
-				unitInfo.addProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
-				unitInfo.addProperty("hibernate.hbm2ddl.auto", "create");
-			}
-
-		};
-
-		cfg.add("${artifactId}", configurer);
-	}
-
-	/**
-	 * Contribute to the Application Defaults
-	 * 
-	 * @param configuration
-	 */
-	public static void contributeApplicationDefaults(MappedConfiguration<String, Object> configuration) {
-		configuration.add(SymbolConstants.PRODUCTION_MODE, false);
-		configuration.add(SymbolConstants.ENABLE_HTML5_SUPPORT, true);
-		configuration.add(SymbolConstants.FORM_CLIENT_LOGIC_ENABLED, false);
-		configuration.add(SymbolConstants.INCLUDE_CORE_STACK, false);
-		configuration.add(SymbolConstants.ENABLE_PAGELOADING_MASK, false);
-		configuration.add(SymbolConstants.COMBINE_SCRIPTS, true);
-		configuration.add(IOCSymbols.THREAD_POOL_CORE_SIZE, 7);
-	}
-
-	/**
-	 * Binds Services
-	 * 
-	 * @param binder
-	 */
-	public static void bind(ServiceBinder binder) {
-
-		binder.bind(AppCore.class, AppCoreImpl.class);
-		binder.bind(PersonResource.class, PersonResourceJPA.class);
-	}
-
-	/**
-	 * REST: Contribute the proxies services for Live Class Reloading
-	 * 
-	 * @param singletons
-	 * @param personResource
-	 */
-	@Contribute(javax.ws.rs.core.Application.class)
-	public static void configureRestResources(Configuration<Object> singletons, PersonResource personResource) {
-		singletons.add(personResource);
-	}
-
-	/**
-	 * REST: To support the @CommitAfter annotation, the *Resource classes are advised wrapping in a transaction
-	 * 
-	 * @param advisor
-	 * @param receiver
-	 */
-	// Uncomment to enable REST Example
-	@Match({ "*Resource" })
-	public static void adviseTransactionally(JpaTransactionAdvisor advisor, MethodAdviceReceiver receiver) {
-		advisor.addTransactionCommitAdvice(receiver);
-	}
+    /**
+     * REST: To support the @CommitAfter annotation, the *Resource classes are advised wrapping in a transaction
+     * 
+     * @param advisor
+     * @param receiver
+     */
+    // Uncomment to enable REST Example
+    @Match({ "*Resource" })
+    public static void adviseTransactionally(JpaTransactionAdvisor advisor, MethodAdviceReceiver receiver) {
+        advisor.addTransactionCommitAdvice(receiver);
+    }
 
 }
---
+++ src/main/resources/archetype-resources/src/main/java/services/UatModeModule.java
@@ -0,0 +1,87 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.services;
+
+import javax.persistence.spi.PersistenceUnitTransactionType;
+
+import org.apache.tapestry5.SymbolConstants;
+import org.apache.tapestry5.ioc.Configuration;
+import org.apache.tapestry5.ioc.IOCSymbols;
+import org.apache.tapestry5.ioc.MappedConfiguration;
+import org.apache.tapestry5.ioc.MethodAdviceReceiver;
+import org.apache.tapestry5.ioc.ServiceBinder;
+import org.apache.tapestry5.ioc.annotations.Contribute;
+import org.apache.tapestry5.ioc.annotations.ImportModule;
+import org.apache.tapestry5.ioc.annotations.Match;
+import org.apache.tapestry5.jpa.EntityManagerSource;
+import org.apache.tapestry5.jpa.JpaTransactionAdvisor;
+import org.apache.tapestry5.jpa.PersistenceUnitConfigurer;
+import org.apache.tapestry5.jpa.TapestryPersistenceUnitInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+//import com.azudio.tapestry.atmosphere.module.AtmosphereModule;
+import ${package}.services.rest.PersonResource;
+import ${package}.services.rest.PersonResourceJPA;
+
+/**
+ * Module to configure your Tapestry Application.
+ */
+@ImportModule({ /* Uncomment to enable Atmosphere: AtmosphereModule.class */})
+public class UatModeModule {
+
+	private static final Logger log = LoggerFactory.getLogger(UatModeModule.class);
+
+	@Contribute(EntityManagerSource.class)
+	public static void configurePersistenceUnitInfos(MappedConfiguration<String, PersistenceUnitConfigurer> cfg) {
+
+		log.debug("Config Persistence");
+
+		PersistenceUnitConfigurer configurer = new PersistenceUnitConfigurer() {
+
+			// /**
+			// * MYSQL Implementation
+			// * @param unitInfo
+			// */
+			// public void configure(TapestryPersistenceUnitInfo unitInfo) {
+			// unitInfo.persistenceProviderClassName("org.hibernate.ejb.HibernatePersistence");
+			// unitInfo.transactionType(PersistenceUnitTransactionType.RESOURCE_LOCAL);
+			// unitInfo.addProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+			// unitInfo.addProperty("hibernate.connection.url", "jdbc:mysql://localhost/uat-${artifactId}");
+			// unitInfo.addProperty("hibernate.connection.username", "${artifactId}"); // <-- CHANGE THIS USERNAME 
+			// unitInfo.addProperty("hibernate.connection.password", "${artifactId}"); // <-- CHANGE THIS PASSWORD
+			// unitInfo.addProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+			// unitInfo.addProperty("hbm2ddl.auto", "create");
+			// }
+
+			/**
+			 * Embedded HSQLDB Implementation
+			 * 
+			 * @param unitInfo
+			 */
+		    @Override
+			public void configure(TapestryPersistenceUnitInfo unitInfo) {
+				unitInfo.persistenceProviderClassName("org.hibernate.ejb.HibernatePersistence");
+				unitInfo.transactionType(PersistenceUnitTransactionType.RESOURCE_LOCAL);
+				unitInfo.addProperty("hibernate.connection.driver_class", "org.hsqldb.jdbcDriver");
+				unitInfo.addProperty("hibernate.connection.url", "jdbc:hsqldb:file:database/uat-${artifactId}");
+				unitInfo.addProperty("hibernate.connection.username", "SA");
+				unitInfo.addProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+				unitInfo.addProperty("hibernate.hbm2ddl.auto", "create");
+			}
+
+		};
+
+		cfg.add("${artifactId}", configurer);
+	}
+
+	/**
+	 * Contribute to the Application Defaults
+	 * 
+	 * @param configuration
+	 */
+	public static void contributeApplicationDefaults(MappedConfiguration<String, Object> configuration) {
+		configuration.add(SymbolConstants.PRODUCTION_MODE, false);
+	}
+
+}
