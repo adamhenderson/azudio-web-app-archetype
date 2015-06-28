518c3bfc938b0cb239c0775fb8d019b50475ece2
Adam Henderson <adamhenderson@azudio.co.uk>
2015-06-28 13:43:52 +0100

    Tidy up DataStoresModule

--- src/main/resources/archetype-resources/src/main/webapp/js/modules/DataStoresModule.js
+++ src/main/resources/archetype-resources/src/main/webapp/js/modules/DataStoresModule.js
@@ -1,13 +1,13 @@
 define([
 
-"dojo/_base/declare", "dstore/Memory", "dstore/Rest", "dstore/Trackable" ], function(
+"dojo/_base/declare", "dstore/Memory", "dstore/Rest" ], function(
 
 declare, Memory, Rest, Trackable
 
 ) {
 
     console.log("DataStoresModule called");
-    
+
     return {
 
         personRestStore : new declare([ Rest, Trackable ])({
@@ -15,19 +15,5 @@ declare, Memory, Rest, Trackable
             sortParam : "sort"
         }),
 
-        // Store for data about how the page is constructed
-        trackableMemoryStore1 : new declare([ Memory, Trackable ])({
-            data : [ {
-                id : 'page',
-                componentDefinition : null,
-                name : 'Person',
-                type : 'page'
-            } ],
-            getChildren : function(object) {
-                return this.query({
-                    parent : this.getIdentity(object)
-                });
-            }
-        })
     }
 });
