8eb9901c8e1bdde3eddab71660756785f25b7f3e
Adam Henderson <adamhenderson@azudio.co.uk>
2015-07-02 06:25:26 +0100

    Updated the REST page so the Grid now updates on adding a new Person

--- src/main/resources/archetype-resources/src/main/webapp/js/modules/Rest.js
+++ src/main/resources/archetype-resources/src/main/webapp/js/modules/Rest.js
@@ -1,15 +1,27 @@
 require([
 
-"dstore/Rest", "dojo/domReady!" ], function(
+"dstore/Memory", "dstore/Rest", "dstore/SimpleQuery", "dstore/Trackable", "dstore/Cache", "dojo/_base/declare", "dojo/on", "dojo/dom", "dojo/request", "dgrid/OnDemandGrid", "dojo/domReady!" ], function(
 
-Rest
+Memory, Rest, SimpleQuery, Trackable, Cache, declare, on, dom, request, OnDemandGrid
 
 ) {
 
     console.debug("Rest Module Called");
 
-    var peopleStore = new Rest({
-        target : './rest/people/'
+    // Define a trackable Memory store
+    var TrackableMemStore = declare([ Memory, Trackable ]);
+
+    // Define a caching Rest store
+    var CachingRestStore = declare([ Rest, Cache, SimpleQuery, Trackable ]);
+
+    // Instantiate a Caching Rest Store that uses a trackable Memory store as the caching store
+
+    // This setup means that any writeable actions on this store will go to the server via rest,
+    // any read actions will hit the cache first and if misses, will then perform a GET request.
+    // The OnDemandGrid will be wired up to the cachingstore to reflect changes.
+    var peopleStore = new CachingRestStore({
+        target : './rest/people/',
+        cachingStore : new TrackableMemStore()
     });
 
     // Using the dstore api to get, search, create and update entities
@@ -24,24 +36,24 @@ Rest
         console.debug("Get Person 1", person);
     });
 
-    // Add new person to the store
+    // Wire up the button to add a new person via the rest store
+    on(dom.byId("addPersonButton"), "click", function(evt) {
+
     peopleStore.add({
         name : "New Person"
     }).then(function(newPerson) {
         console.debug("A New Person", newPerson);
     });
 
-    // Add new person to the store
-    peopleStore.put({
-        id : 1,
-        name : "Updated" + new Date().getTime()
-    }).then(function(newPerson) {
-        console.debug("Updated a Person", newPerson);
-
-        peopleStore.fetch().then(function(people) {
-            console.debug("People returned", people);
+        console.log(personGrid);
         });
 
-    });
+    var personGrid = new OnDemandGrid({
+        collection : peopleStore.cachingStore,
+        columns : {
+            id : 'ID',
+            name : 'Name'
+        }
+    }, "person-grid");
 
 });
